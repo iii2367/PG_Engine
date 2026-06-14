d0a2f54b8f08f0b0507727749e499f8db8999e51
rukiamuq-hard <kusagaid@gmail.com>
2026-06-14 15:42:27 +0300

    Added Getter for window size method

--- src/platform/IWindowPlatform.h
+++ src/platform/IWindowPlatform.h
@@ -3,15 +3,15 @@
 
 #include <string>
 
-struct IWindowPlatform
-{
-    virtual ~IWindowPlatform() = default;
+struct IWindowPlatform {
+  virtual ~IWindowPlatform() = default;
 
-    virtual bool createWindow(int width, int height, std::string title) = 0;
-    virtual void renderFrame() = 0;
-    virtual void destroyWindow() = 0;
-    virtual void setTitle(std::string title) = 0;
-    virtual void resize(int width, int height) = 0;
+  virtual bool createWindow(int width, int height, std::string title) = 0;
+  virtual void renderFrame() = 0;
+  virtual void destroyWindow() = 0;
+  virtual void getWindowSize(int &w, int &h) = 0;
+  virtual void setTitle(std::string title) = 0;
+  virtual void resize(int width, int height) = 0;
 };
 
 #endif
--- src/platform/SDL3Platform/SDL3WindowPlatform/SDL3WindowPlatform.h
+++ src/platform/SDL3Platform/SDL3WindowPlatform/SDL3WindowPlatform.h
@@ -5,17 +5,17 @@
 
 #include <SDL3/SDL.h>
 
-class SDL3WindowPlatform : public IWindowPlatform
-{
+class SDL3WindowPlatform : public IWindowPlatform {
 public:
-    SDL_Window* window = nullptr;
-    SDL_Renderer *render = nullptr;
+  SDL_Window *window = nullptr;
+  SDL_Renderer *render = nullptr;
 
-    bool createWindow(int width, int height, std::string title) override;
-    void destroyWindow() override;
-    void renderFrame() override;
-    void setTitle(std::string title) override;
-    void resize(int width, int height) override;
+  bool createWindow(int width, int height, std::string title) override;
+  void destroyWindow() override;
+  void renderFrame() override;
+  void getWindowSize(int &w, int &h) override;
+  void setTitle(std::string title) override;
+  void resize(int width, int height) override;
 };
 
 #endif
