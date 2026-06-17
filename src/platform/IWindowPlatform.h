9e56d73bad7a9ab48cc1ab2f8610edc6a3cb7293
rukiamuq-hard <kusagaid@gmail.com>
2026-06-17 23:23:19 +0300

    Added methods to window platform

--- src/platform/IWindowPlatform.h
+++ src/platform/IWindowPlatform.h
@@ -1,30 +1,42 @@
 #ifndef IWINDOWPLATFORM_H
 #define IWINDOWPLATFORM_H
 
-#include <string>
 #include "WindowHandle.h"
+#include <string>
 
-struct WindowInfo
-{
-    int width{800};
-    int height{600};
-    std::string title{"Window"};
+struct WindowInfo {
+  int width{800};
+  int height{600};
+  std::string title{"Window"};
 
-    bool enableRender{false};   
-    bool resizable{false};      
-    bool fullscreen{false};        
-    bool decorated{true};
+  bool enableRender{false};
+  bool resizable{false};
+  bool fullscreen{false};
+  bool decorated{true};
 };
 
 struct IWindowPlatform {
   virtual ~IWindowPlatform() = default;
 
-  virtual bool createWindow(const WindowInfo& info) = 0;
+  virtual bool createWindow(const WindowInfo &info) = 0;
   virtual bool renderFrame() = 0;
   virtual void destroyWindow() = 0;
   virtual void getWindowSize(int &w, int &h) = 0;
   virtual void setTitle(std::string title) = 0;
   virtual void resize(int width, int height) = 0;
+
+  virtual void setWindowSize(int x, int y) = 0;
+  virtual const char *getTitle() = 0;
+  virtual void setFullScreen(bool enabled) = 0;
+  virtual bool isFullScreen() = 0;
+  virtual void show() = 0;
+  virtual void hide() = 0;
+  virtual bool isVisible() = 0;
+  virtual void setCursorVisible(bool visible) = 0;
+  virtual bool isCursorVisible() = 0;
+  virtual void setCursorPosition(float x, float y) = 0;
+  virtual void getCursorPosition(float &x, float &y) = 0;
+
   virtual WindowHandle getWindowHandle() = 0;
 };
 
--- src/platform/SDL3Platform/SDL3WindowPlatform/SDL3WindowPlatform.cpp
+++ src/platform/SDL3Platform/SDL3WindowPlatform/SDL3WindowPlatform.cpp
@@ -1,26 +1,40 @@
 #include "SDL3WindowPlatform.h"
+#include <SDL3/SDL_mouse.h>
 #include <SDL3/SDL_platform.h>
+#include <SDL3/SDL_stdinc.h>
 #include <SDL3/SDL_video.h>
 
-bool SDL3WindowPlatform::createWindow(const WindowInfo& info) {
-    if (window || render) { return false; }
-    
-    Uint32 flags = 0;
-    if (info.resizable) { flags |= SDL_WINDOW_RESIZABLE; }
-    if (info.fullscreen) { flags |= SDL_WINDOW_FULLSCREEN; }
-    if (!info.decorated) { flags |= SDL_WINDOW_BORDERLESS; }
-    
-    window = SDL_CreateWindow(info.title.c_str(), info.width, info.height, flags);
-    if (!window) { SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError()); return false; }
-    
-    if (info.enableRender) { 
-        render = SDL_CreateRenderer(window, nullptr); 
-        if (!render) { SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
-            SDL_DestroyWindow(window);
-            window = nullptr;
-            return false;
-        }
+bool SDL3WindowPlatform::createWindow(const WindowInfo &info) {
+  if (window || render) {
+    return false;
+  }
+
+  Uint32 flags = 0;
+  if (info.resizable) {
+    flags |= SDL_WINDOW_RESIZABLE;
+  }
+  if (info.fullscreen) {
+    flags |= SDL_WINDOW_FULLSCREEN;
+  }
+  if (!info.decorated) {
+    flags |= SDL_WINDOW_BORDERLESS;
+  }
+
+  window = SDL_CreateWindow(info.title.c_str(), info.width, info.height, flags);
+  if (!window) {
+    SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+    return false;
+  }
+
+  if (info.enableRender) {
+    render = SDL_CreateRenderer(window, nullptr);
+    if (!render) {
+      SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
+      SDL_DestroyWindow(window);
+      window = nullptr;
+      return false;
     }
+  }
   return true;
 }
 
@@ -32,7 +46,9 @@ void SDL3WindowPlatform::destroyWindow() {
 }
 
 bool SDL3WindowPlatform::renderFrame() {
-  if (!render) { return false; }
+  if (!render) {
+    return false;
+  }
   SDL_SetRenderDrawColor(render, 128, 128, 128, 255);
   SDL_RenderClear(render);
   SDL_RenderPresent(render);
@@ -58,9 +74,57 @@ void SDL3WindowPlatform::getWindowSize(int &w, int &h) {
 }
 
 WindowHandle SDL3WindowPlatform::getWindowHandle() {
-    WindowHandle wh{};
-    wh.type = TypeWindowHandle::SDL3;
-    wh.handle = window;
-    getWindowSize(wh.width, wh.height);
-    return wh;
+  WindowHandle wh{};
+  wh.type = TypeWindowHandle::SDL3;
+  wh.handle = window;
+  getWindowSize(wh.width, wh.height);
+  return wh;
+}
+
+void SDL3WindowPlatform::setWindowSize(int x, int y) {
+  SDL_SetWindowSize(window, x, y);
+}
+
+const char *SDL3WindowPlatform::getTitle() {
+  return SDL_GetWindowTitle(window);
+}
+
+void SDL3WindowPlatform::setFullScreen(bool enabled) {
+  SDL_SetWindowFullscreen(window, enabled);
+}
+
+bool SDL3WindowPlatform::isFullScreen() {
+  Uint64 flags = SDL_GetWindowFlags(window);
+  return (flags & SDL_WINDOW_FULLSCREEN);
+}
+
+void SDL3WindowPlatform::show() { SDL_ShowWindow(window); }
+
+void SDL3WindowPlatform::hide() { SDL_HideWindow(window); }
+
+bool SDL3WindowPlatform::isVisible() {
+  Uint64 flags = SDL_GetWindowFlags(window);
+  return !(flags & SDL_WINDOW_HIDDEN);
+}
+
+void SDL3WindowPlatform::setCursorVisible(bool visible) {
+  if (visible)
+    SDL_ShowCursor();
+  else
+    SDL_HideCursor();
+}
+
+bool SDL3WindowPlatform::isCursorVisible() {
+  if (SDL_CursorVisible())
+    return true;
+  else
+    return false;
+}
+
+void SDL3WindowPlatform::getCursorPosition(float &x, float &y) {
+  SDL_GetMouseState(&x, &y);
+}
+
+void SDL3WindowPlatform::setCursorPosition(float x, float y) {
+  SDL_WarpMouseInWindow(window, x, y);
 }
